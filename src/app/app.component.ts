b4f8571434170afd555f4a363a80108db9a006b3
Sippora04 <sippora.toppo95@gmail.com>
2020-12-25 18:18:26 +0530

    Added: using event binding texhnique to bind Click Event to launch new site in a new Tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld';
-  imgUrl="../assets/BridgeLabz New Logo.svg";
+  imgUrl = "../assets/BridgeLabz New Logo.svg";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz!!";
+  }
+
+  onClick($event) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
