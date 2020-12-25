c816ff9309d8aa947c17eb253f59de61e1b33316
Sippora04 <sippora.toppo95@gmail.com>
2020-12-25 18:56:38 +0530

    Added: validation on user input name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   url = "https://www.bridgelabz.com";
   //Specify userName as String Type
   userName: string = "";
+  nameError: string = "";
 
   ngOnInit(): void {
     this.title = "Hello from Bridgelabz!!";
@@ -19,5 +20,15 @@ export class AppComponent {
   onClick($event) {
     console.log("Save button is clicked!", $event);
     window.open(this.url, "_blank");
+  }
+
+  onInput($event) {
+    console.log("Change Event Occured! ", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect!";
   }
 }
